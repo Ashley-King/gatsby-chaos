72c2b9e74700e557877ddb2172f4f020ef1c89ce
Ashley King <ashleyot@gmail.com>
2019-05-05 14:14:32 -0400

    images page now getting image name from contentful

--- src/pages/images.js
+++ src/pages/images.js
@@ -5,27 +5,46 @@ import {graphql} from 'gatsby'
 
 export default function Images({data}) {
   
- const imgs = data.allFile.edges;
+//  const imgs = data.allFile.edges;
+ const imgs = data.allContentfulAsset.edges;
+ console.log(data)
  return (
     <Layout>
-     <h5>All Images</h5>
+     <h5>All Image Names in Contentful</h5>
       {imgs.map(({node}) => (
-        <h6 key={node.id}>{node.relativePath}</h6>
+        <h6 key={node.id}>{node.file.fileName}</h6>
       ))}
-     
+    
     </Layout>
   )
 }
 
 export const query = graphql`
-  {
-   allFile(filter:{dir:{eq: "/Users/ashley/Desktop/gat/src/images"}}){
-      edges{
-        node{
-          relativePath
+  
+    {
+  allContentfulAsset {
+    edges {
+      node {
+        id
+        file {
+          
+          fileName
         }
+        
       }
     }
   }
- 
+}
+
 `
+// old query from local path - can't use relative paths with netlify in production
+
+// {
+//   allFile(filter:{dir:{eq: "/Users/ashley/Desktop/gat/src/images"}}){
+//      edges{
+//        node{
+//          relativePath
+//        }
+//      }
+//    }
+//  }
